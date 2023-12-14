3360aa86335c3052cdab31e7c60dbd2ee72489a6
XjorLml <lemnuellumaban@gmail.com>
2023-12-14 21:13:47 +0800

    uploading chat app

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../models/chatUsersModel.dart';
+import '../widgets/conversationList.dart';
+import 'chatDetailPage.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -6,6 +9,48 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatUsers> chatUsers = [
+    ChatUsers(
+        name: "Jane Russel",
+        messageText: "Awesome Setup",
+        imageURL:"https://i.pinimg.com/236x/d0/f9/f3/d0f9f3c1289d4122cd0969d32b4cb855.jpg",
+        time: "Now"),
+    ChatUsers(
+        name: "Glady's Murphy",
+        messageText: "That's Great",
+        imageURL: "https://i.pinimg.com/236x/96/db/67/96db67850f26204391d9f14dbc028f40.jpg",
+        time: "Yesterday"),
+    ChatUsers(
+        name: "Caloocan Boy",
+        messageText: "Hey where are you?",
+        imageURL: "https://i.pinimg.com/564x/0e/64/81/0e64818890168567c30466e1fd923d10.jpg",
+        time: "31 Mar"),
+    ChatUsers(
+        name: "Philip Fox",
+        messageText: "Busy! Call me in 20 mins",
+        imageURL: "https://i.pinimg.com/236x/b4/3a/89/b43a892e3f68c50a5b7ce996aa41a1af.jpg",
+        time: "28 Mar"),
+    ChatUsers(
+        name: "Debra Hawkins",
+        messageText: "Thankyou, It's awesome",
+        imageURL: "https://i.pinimg.com/236x/3a/57/6b/3a576b0f3c5d80172ba13208290b550c.jpg",
+        time: "23 Mar"),
+    ChatUsers(
+        name: "Jacob Pena",
+        messageText: "will update you in the evening",
+        imageURL: "https://i.pinimg.com/236x/1d/26/74/1d2674168fc2f73000121ea2f56ac9c5.jpg",
+        time: "17 Mar"),
+    ChatUsers(
+        name: "Andrey Jones",
+        messageText: "Can you please share the file?",
+        imageURL: "https://i.pinimg.com/236x/11/4a/8e/114a8edfa19d959f13cdb6f3d15b6830.jpg",
+        time: "24 Feb"),
+    ChatUsers(
+        name: "Teressa Wiggins",
+        messageText: "How are you?",
+        imageURL: "https://i.pinimg.com/236x/32/01/72/32017207d0bf9b00a27cd6d5a2a05fbd.jpg",
+        time: "18 Feb"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,6 +119,21 @@ class _ChatPageState extends State<ChatPage> {
                       borderSide: BorderSide(color: Colors.grey.shade100)),
                 ),
               ),
+            ),
+            ListView.builder(
+              itemCount: chatUsers.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 16),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ConversationList(
+                  name: chatUsers[index].name,
+                  messageText: chatUsers[index].messageText,
+                  imageUrl: chatUsers[index].imageURL,
+                  time: chatUsers[index].time,
+                  isMessageRead: (index == 0 || index == 3) ? true : false,
+                );
+              },
             ),
           ],
         ),
